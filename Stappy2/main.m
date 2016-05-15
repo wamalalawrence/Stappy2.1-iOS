1fd5040d484b8ff157dfb225908d3ad391dd626c
Thorsten Binnewies <thorsten.binnewies@visualication.de>
2016-05-15 10:52:01 +0200

    Initial Commit

---
+++ Stappy2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Stappy2
+//
+//  Created by Thorsten Binnewies on 15.05.16.
+//  Copyright © 2016 endios GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
